b9c4b6f7aa83e312489caaa1963a103751e75849
Tatiane Souza <tatianefx@gmail.com>
2023-01-15 19:12:19 -0300

    Create project

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Tatiane Souza on 15/01/23.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
